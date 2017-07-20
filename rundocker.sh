1a01e4ce7c72d20080dced0d8bb4502a2bd54185
Steve Cassidy <steve.cassidy@mq.edu.au>
2017-07-20 17:49:06 +1000

    Add shell script to run a docker container for jupyter

---
+++ rundocker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# run the jupyter notebook server via a docker container, mounting the current
+# directory so that we can browse and run the containers
+docker run -it --rm -v `pwd`:/home/jovyan/ -p 8888:8888 jupyter/datascience-notebook
+
+
+# see https://github.com/jupyter/docker-stacks/tree/master/datascience-notebook
+# for further options with this docker container
